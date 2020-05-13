a520a2c3dee728ecbf495b76c477074ce0536a5a
mymislife <mymislife@gmail.com>
2020-05-13 17:30:23 +0100

    git init

---
+++ src/Company.ts
@@ -0,0 +1,26 @@
+import * as faker from "faker";
+import {Mappable} from "./CustomMap";
+
+export class Company implements Mappable{
+    name: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number;
+    }
+
+    color: string = 'red';
+
+    constructor() {
+        this.name = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+
+    markerContent(): string{
+        return `Company Name: ${this.name}`;
+    }
+}
